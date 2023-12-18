54cf694a223061541dc1dcf7fef9f837b7c04157
Arthur Eustaquio <124733752+Artses@users.noreply.github.com>
2023-12-18 01:17:09 -0300

    initial commit

---
+++ Interface/src/Service/ContractService.java
@@ -0,0 +1,25 @@
+package Service;
+
+import Entities.Contract;
+import Entities.Installment;
+
+import java.time.LocalDate;
+
+public class ContractService {
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract(Contract contract, Integer months){
+            double basicQuota = contract.getTotalValue() / months;
+        for (int i = 1; i <= months; i++) {
+            LocalDate dueDate = contract.getDate().plusMonths(i);
+            double interest = onlinePaymentService.interest(basicQuota, i);
+            double fee = onlinePaymentService.paymentFee(basicQuota + interest);
+            double quota = basicQuota + interest + fee;
+            contract.getInstallments().add(new Installment(dueDate, quota));
+        }
+    }
+}
